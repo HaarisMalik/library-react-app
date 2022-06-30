069a9542a5a3a54a7a5799b198da4558f1a393bf
HaarisMalik <haaris.malik416@gmail.com>
2022-06-29 20:15:13 -0400

    mulit-page/route/app

--- src/App.js
+++ src/App.js
@@ -1,57 +1,107 @@
-import Nav from './components/Nav';
-import Home from './pages/Home';
-import Books from './pages/Books';
-import Footer from './components/Footer';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { books } from './data';
-import BookInfo from './pages/BookInfo';
-import Cart from './pages/Cart';
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import BookInfo from "./pages/BookInfo";
+import { books } from "./data";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Cart from "./pages/Cart";
 
 
 function App() {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
-  function addToCart(book) {
-        setCart([...cart, book])
-
-   /*const dupeItem = cart.find(item => +item.id === +book.id)
-   if (dupeItem) {
-   setCart(cart.map(item => {
-    if (item.id === dupeItem.id) {
-       return {
-        ...item,
-        quantity: item.quantity + 1,
-       }
-    }
-    else {
-      return item
-    }
-   })
-   )
-   }
-   else {
-    setCart([...cart, { ...book, quantity: 1 }])
-   }*/
-
-  
+  function addItemToCart(book) {
+    const dupeItem = cart.find((item) => item.id === book.id);
+    setCart((oldCart) =>
+      dupeItem
+        ? [
+            ...oldCart.map((item) => {
+              return item.id === dupeItem.id
+                ? {
+                    ...item,
+                    quantity: item.quantity + 1,
+                  }
+                : item;
+            }),
+          ]
+        : [...oldCart, { ...book, quantity: 1 }]
+    );
   }
 
-  
-  useEffect(() => {
-    console.log(cart)
-  }, [cart])
+  function updateCart(item, newQuantity) {
+    setCart((oldCart) =>
+      oldCart.map((oldItem) => {
+        if (oldItem.id === item.id) {
+          return {
+            ...oldItem,
+            quantity: newQuantity,
+          };
+        } else {
+          return oldItem;
+        }
+      })
+    );
+  }
+
+  function removeItem(item) {
+    setCart((oldCart) => oldCart.filter((cartItem) => cartItem.id !== item.id));
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach((item) => {
+      counter += +item.quantity;
+    });
+    return counter;
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach((item) => {
+      counter += +item.quantity;
+    });
+    return counter;
+  }
+
+  function calcPrices() {
+    let total = 0;
+    cart.forEach((item) => {
+      total += (item.salePrice || item.originalPrice) * item.quantity;
+    });
+    return {
+      subtotal: total * 0.9,
+      tax: total * 0.1,
+      total,
+    };
+  }
 
   return (
     <Router>
-    <div className="App">
-     <Nav/>
-     <Route path="/" exact component={Home} />
-     <Route path="/books" exact render={() => <Books books={books} /> } />
-     <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} cart={cart} /> } />
-     <Route path="/cart" render={() => <Cart books={books} cart={cart} /> } />
-     <Footer/>
-    </div>
+      <div className="App">
+        <Nav numberOfItems={numberOfItems()} />
+        <Route path="/" exact render={() => <Home books={books} />} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => (
+            <BookInfo books={books} addItemToCart={addItemToCart} />
+          )}
+        />
+        <Route
+          path="/cart"
+          render={() => (
+            <Cart
+              cart={cart}
+              updateCart={updateCart}
+              removeItem={removeItem}
+              totals={calcPrices()}
+            />
+          )}
+        />
+        <Footer />
+      </div>
     </Router>
   );
 }
--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,30 +1,20 @@
 import React from "react";
-import Book from "./ui/Book";
-import { books } from '../data'
+import Book from "./Book";
+import BestBooks from "./ui/BestBooks";
 
 const Featured = () => {
-    console.log(books)
-
-    return (
-       <section id="features">
-        <div className="container">
-            <div className="row">
-                <h2 className="section__title">
-                    Featured <span className="purple">Books</span>
-                </h2>
-                <div className="books">
-                {books
-                .filter((book) => book.rating === 5)
-                .slice(0, 4)
-                .map((book) => (<Book book={book} key={book.id}/>
-                ))
-                }
-        
-                </div>
-            </div>
+  return (
+    <section id="features">
+      <div className="container">
+        <div className="row">
+          <h2 className="section__title">
+            Featured <span className="purple">Books</span>
+          </h2>
+          <BestBooks />
         </div>
-       </section>
-    )
-}
+      </div>
+    </section>
+  );
+};
 
-export default Featured
+export default Featured;
